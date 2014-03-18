a5848fc06212ee069ec525158f66fe25445aacc1
s093018 <christianwnielsen@gmail.com>
2014-03-18 16:34:09 +0100

    created new classes

---
+++ src/system/Robot.java
@@ -0,0 +1,28 @@
+/**
+ * 
+ */
+package system;
+
+/**
+ * @author Christian W. Nielsen - s093018
+ *
+ */
+public class Robot {
+
+	public void forward () {
+		
+	}
+	
+	public void backward () {
+		
+	}
+	
+	public void left () {
+		
+	}
+	
+	public void right () {
+		
+	}
+	
+}
