ce20f3f3812e068d063e593fc4978f01c25596bb
Alexander <Alexander@Alex-brbar>
2014-04-30 12:08:54 +0200

    update() udfyldt.

--- src/imageProcessing/RealCamera.java
+++ src/imageProcessing/RealCamera.java
@@ -3,9 +3,13 @@ package imageProcessing;
 import java.util.List;
 
 import org.opencv.core.Core;
+import org.opencv.core.Core.MinMaxLocResult;
+import org.opencv.core.CvType;
 import org.opencv.core.Mat;
+import org.opencv.core.Scalar;
 import org.opencv.highgui.Highgui;
 import org.opencv.highgui.VideoCapture;
+import org.opencv.imgproc.Imgproc;
 
 public class RealCamera implements Camera
 {
@@ -28,7 +32,7 @@ public class RealCamera implements Camera
 			return frame;
 		}
 	}
-	
+
 	public RealCamera()
 	{
 		System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
@@ -43,11 +47,37 @@ public class RealCamera implements Camera
 
 		update();//Find bolde + robot
 	}
-	
+
 	public void update()
 	{
-		getImage();
-		//opdater balls, robot
+		Mat image = getImage();
+		Mat templ = Highgui.imread("../imgInOut/Template.jpg");
+
+		int result_cols = image.cols() - templ.cols() + 1;
+		int result_rows = image.rows() - templ.rows() + 1;
+		Mat result = new Mat(result_rows, result_cols, CvType.CV_32FC1);
+
+		while(true){
+			int matchingMethod = 1;
+			Imgproc.matchTemplate(image, templ, result, matchingMethod);
+
+			MinMaxLocResult mmlr = Core.minMaxLoc(result);
+
+			org.opencv.core.Point matchLoc;
+			if( matchingMethod  == Imgproc.TM_SQDIFF || matchingMethod == Imgproc.TM_SQDIFF_NORMED ){
+				matchLoc = mmlr.minLoc; }
+			else{ 
+				matchLoc = mmlr.maxLoc; }
+
+			double thresholdMatch = 0.25;
+			if(mmlr.minVal < thresholdMatch){
+				Core.circle(image, new org.opencv.core.Point(matchLoc.x + (templ.cols()/2),
+						matchLoc.y + (templ.rows()/2)), 6, new Scalar(0, 0, 255), -1); // -1 = fill)
+			}
+			else
+				break;
+		}
+		Highgui.imwrite("../imgInOut/Result.jpg", image);
 	}
 
 	//optimer de her senere hvis det bliver nødvendigt
