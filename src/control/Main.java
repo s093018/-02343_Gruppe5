fe7f342f2a251904278eb0b434fe4d24a720d7c5
s093018 <christianwnielsen@gmail.com>
2014-06-20 12:40:06 +0200

    test v.1.1

--- src/control/Main.java
+++ src/control/Main.java
@@ -11,10 +11,10 @@ public class Main {
 	 * Main method for test cases:
 	 */
 	public static void main(String[] arg) {
-		ControllerFinal ctrl = new ControllerFinal();
+//		ControllerFinal ctrl = new ControllerFinal();
 	
 //		ControllerRobot ctrl = new ControllerRobot();
-//		ControllerBoard ctrl = new ControllerBoard();
+		ControllerBoard ctrl = new ControllerBoard();
 		ctrl.run();
 	}
 }
